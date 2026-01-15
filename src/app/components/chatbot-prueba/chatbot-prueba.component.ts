2a55554ff96e9545138e696b3693a282e91b1be9
lenovo <monica.romero.z@tecsup.edu.pe>
2026-01-15 13:27:09 -0500

    actualizar iagquu

--- src/app/components/chatbot-prueba/chatbot-prueba.component.ts
+++ src/app/components/chatbot-prueba/chatbot-prueba.component.ts
@@ -22,7 +22,7 @@ export class ChatbotPruebaComponent implements OnInit {
   isLoading = false;
 
   // ✅ TU BACKEND EN RENDER (Groq)
-private API_URL = 'https://backend-dinsac-hlf0.onrender.com/chatia';
+private API_URL = 'https://backend-dinsac-77sq.onrender.com/chatia';
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
